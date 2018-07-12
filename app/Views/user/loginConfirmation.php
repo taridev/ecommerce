1ec48056d19157b839c096fb2ff8405cb89bf631
Matthieu Audemard <33572957+taridev@users.noreply.github.com>
2018-07-12 15:43:55 +0200

    ajout de page de confirmation pour login et register

---
+++ app/Views/user/loginConfirmation.php
@@ -0,0 +1,6 @@
+<div class="container">
+
+    <h2>Bonjour <?= \App\Service\AuthService::username(); ?></h2>
+    <p><a href="index.php">retournez à l'accueil</a></p>
+
+</div>
---
+++ app/Views/user/registerConfirmation.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+    <h2>Félicitation, votre compte a bien été créé.</h2>
+    <p>Vous êtes à présent identifié en tant que <?= \App\Service\AuthService::username(); ?></p>
+    <p><a href="index.php">retournez à l'accueil</a></p>
+
+</div>
